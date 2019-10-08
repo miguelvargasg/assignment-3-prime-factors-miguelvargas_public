48b084e0f2bdf65e1406239f9301a016667e7075
Miguel Vargas <mfvg1920@gmail.com>
2019-10-07 22:38:32 -0400

    Step 2 Corrected

--- prime.py
+++ prime.py
@@ -7,3 +7,14 @@ def generate_prime_factors(n):
 
     if type(n) is not int:
         raise ValueError
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -4,5 +4,6 @@ from prime import generate_prime_factors
 """
 The test module for Prime Factors
 """
-def test_prime_factor():
-    pass
+def test_prime_factor_1():
+    primes = []
+    assert generate_prime_factors(1) == primes
