963af9e89224a1bed941df67c881be17739229a4
Miguel Vargas <mfvg1920@gmail.com>
2019-10-07 22:40:20 -0400

    Step 3 Corrected

--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -7,3 +7,7 @@ The test module for Prime Factors
 def test_prime_factor_1():
     primes = []
     assert generate_prime_factors(1) == primes
+
+def test_prime_factor_2():
+    primes = [2]
+    assert generate_prime_factors(2) == primes
