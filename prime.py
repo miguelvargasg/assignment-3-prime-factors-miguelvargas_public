ea917dc711229b7c46a0dbce6fa9752bff9895a4
Miguel Vargas <mfvg1920@gmail.com>
2019-10-07 20:30:16 -0400

    Step 1

--- prime.py
+++ prime.py
@@ -1,4 +1,10 @@
+import sys
 """
 prime.py -- Write the application code here
 """
-# thirdAssignment 
+# thirdAssignment
+def generate_prime_factors(p, primes):
+    primes = []
+    if type(p) is not int:
+        raise ValueError
+    return primes
--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -1,4 +1,13 @@
 # pylint: disable=missing-docstring
+import pytest
+from prime import generate_prime_factors
 """
 The test module for Prime Factors
 """
+def test_prime_factor():
+    """
+    Given a prime number
+    """
+    #with pytest.raises(ValueError):
+    primes = []
+    #assert generate_prime_factors(21, primes) == 21
